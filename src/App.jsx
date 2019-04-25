9e39391c75b2c09500172ac270e2fec432dd3658
WinfredZhuwenfeng <1419657141@qq.com>
2019-04-25 16:46:25 +0800

    'react-webpack-init'

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+import { hot } from 'react-hot-loader/root'
+
+class App extends React.Component{
+  render(){
+    return (
+
+      <div>
+        <h3>hello1</h3>
+      </div>
+      )
+  }
+}
+
+export default hot(App)
---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  resolve: {
+    // 别名
+    alias: {
+      "@": path.resolve(__dirname,"src/components")
+    },
+    // 拓展名
+    extensions: [".jsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+    ]
+}
